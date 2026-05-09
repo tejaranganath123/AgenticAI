50cfd7417d4d8ab66310fd2f3c661c2401e4ae0f
tejaranganath123 <tejaranganath2@gmail.com>
2026-05-09 12:31:55 +0530

    creds

--- LangChain1/Project1/app.py
+++ LangChain1/Project1/app.py
@@ -16,8 +16,11 @@ logging.getLogger("transformers").setLevel(logging.ERROR)
 
 load_dotenv()
 
-## Langsmith Tracking - get these  while running the code to track the interactions with the model in langsmith dashboard
-
+## Langsmith Tracking
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"    
+os.environ["LANGSMITH_API_KEY"] = "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"
+os.environ["LANGSMITH_PROJECT"] = "Test"
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
